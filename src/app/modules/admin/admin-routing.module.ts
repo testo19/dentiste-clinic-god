262d26568869140fdcaee84b5764ec71d0e29c37
testo19 <siwakon-mos@outlook.com>
2022-05-06 00:15:03 +0700

    V1

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { UsersManageComponent } from './components/users-manage/users-manage.component';
+import { UserRegisterComponent } from './components/users-register/user-register.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,7 @@ const routes: Routes = [
     component: AdminDashboardComponent,
   },
   { path: 'users-manage', component: UsersManageComponent },
+  { path: 'users-register', component: UserRegisterComponent },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { UsersManageComponent } from './components/users-manage/users-manage.component';
+import { UserRegisterComponent } from './components/users-register/user-register.component';
 
 
 @NgModule({
   declarations: [
     AdminDashboardComponent,
-    UsersManageComponent
+    UsersManageComponent,
+    UserRegisterComponent
   ],
   imports: [
     CommonModule,
